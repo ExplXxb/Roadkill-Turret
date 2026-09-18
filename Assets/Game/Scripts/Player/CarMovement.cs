f627655ea127e5ef18eb508abe557739c40e55e7
ExplXxb <n.zemsckij@gmail.com>
2026-09-18 22:49:11 +0300

    feat: wire up CarMovement and GameFlowController via VContainer

--- Assets/Game/Scripts/Player/CarMovement.cs
+++ Assets/Game/Scripts/Player/CarMovement.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using VContainer;
 
 public class CarMovement : MonoBehaviour
 {
-    [SerializeField] GameFlowController _gameFlowController;
-
     [SerializeField] private float _moveSpeed = 10.0f;
     [SerializeField] private Rigidbody _rigidbody;
 
+    GameFlowController _gameFlowController;
     private bool _isMoving = false;
 
-    public void StartMoving() => _isMoving = true;
-    public void StopMoving() => _isMoving = false;
+    [Inject]
+    public void Construct(GameFlowController gameFlowController)
+    {
+        _gameFlowController = gameFlowController;
+    }
+    private void OnValidate()
+    {
+        if (_rigidbody == null)
+            _rigidbody = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
@@ -22,16 +30,12 @@ public class CarMovement : MonoBehaviour
         _gameFlowController.OnGameStarted -= StartMoving;
     }
 
-    private void OnValidate()
-    {
-        if (_rigidbody == null)
-            _rigidbody = GetComponent<Rigidbody>();
-    }
-
     private void FixedUpdate()
     {
         if (!_isMoving) return;
         _rigidbody.MovePosition(_rigidbody.position + transform.forward * _moveSpeed * Time.fixedDeltaTime);
     }
 
+    public void StartMoving() => _isMoving = true;
+    public void StopMoving() => _isMoving = false;
 }
