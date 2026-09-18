3788fcb5769b1e664cfd29a03e082436248441c4
ExplXxb <n.zemsckij@gmail.com>
2026-09-18 22:18:41 +0300

    feat: add CarMovement script for forward movement via Rigidbody

---
+++ Assets/Game/Scripts/Player/CarMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 10.0f;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    private bool _isMoving = false;
+
+    public void StartMoving() => _isMoving = true;
+    public void StopMoving() => _isMoving = false;
+
+    private void OnValidate()
+    {
+        if (_rigidbody == null)
+            _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isMoving) return;
+        _rigidbody.MovePosition(_rigidbody.position + transform.forward * _moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
