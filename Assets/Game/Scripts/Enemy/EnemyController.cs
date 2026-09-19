b3daf1808d7a8f3067d9deb0caa5d40f411a315a
ExplXxb <n.zemsckij@gmail.com>
2026-09-19 21:32:55 +0300

    feat: add EnemyAttack and wire up death/attack handling in EnemyController

--- Assets/Game/Scripts/Enemy/EnemyController.cs
+++ Assets/Game/Scripts/Enemy/EnemyController.cs
@@ -4,12 +4,34 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] private EnemyAggro _aggroTrigger;
     [SerializeField] private EnemyMovement _movement;
+    [SerializeField] private EnemyAttack _attack;
+    [SerializeField] private Health _health;
 
-    private void OnEnable() => _aggroTrigger.OnPlayerDetected += HandlePlayerDetected;
-    private void OnDisable() => _aggroTrigger.OnPlayerDetected -= HandlePlayerDetected;
+    private void OnEnable()
+    {
+        _aggroTrigger.OnPlayerDetected += HandlePlayerDetected;
+        _attack.OnAttacked += HandleAttacked;
+        _health.OnDied += HandleDied;
+    }
+
+    private void OnDisable()
+    {
+        _aggroTrigger.OnPlayerDetected -= HandlePlayerDetected;
+        _attack.OnAttacked -= HandleAttacked;
+        _health.OnDied -= HandleDied;
+    }
 
     private void HandlePlayerDetected(Car car)
     {
         _movement.SetTarget(car.transform);
+    }
+
+    private void HandleAttacked() => Die();
+    private void HandleDied() => Die();
+
+    private void Die()
+    {
+        _movement.Stop();
+        Destroy(gameObject);
     }
 }
