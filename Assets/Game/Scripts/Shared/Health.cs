bf74a996effc16e08d1c48cf3409e47247b2f36a
ExplXxb <n.zemsckij@gmail.com>
2026-09-19 18:45:35 +0300

    feat: add Health component implementing IDamageable

---
+++ Assets/Game/Scripts/Shared/Health.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour, IDamageable
+{
+    [SerializeField] private int _maxHealth = 20;
+
+    public event Action OnDied;
+    public event Action<int> OnHealthChanged;
+
+    private int _currentHealth;
+
+    private void Awake() => _currentHealth = _maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        _currentHealth = Mathf.Max(0, _currentHealth - amount);
+        OnHealthChanged?.Invoke(_currentHealth);
+
+        if (_currentHealth <= 0)
+            OnDied?.Invoke();
+    }
+}
