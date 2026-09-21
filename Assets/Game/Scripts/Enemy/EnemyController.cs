f223a5bb279aefa731969966ad5357a8630d6565
ExplXxb <n.zemsckij@gmail.com>
2026-09-21 20:54:26 +0300

    fix: update enemy attack logic and fix event unsubscription typo

--- Assets/Game/Scripts/Enemy/EnemyController.cs
+++ Assets/Game/Scripts/Enemy/EnemyController.cs
@@ -33,7 +33,7 @@ public class EnemyController : MonoBehaviour
     {
         _aggroTrigger.OnPlayerDetected -= HandlePlayerDetected;
         _attack.OnAttacked -= HandleAttacked;
-        _health.OnHealthChanged += HandleTakeDamage;
+        _health.OnHealthChanged -= HandleTakeDamage;
         _health.OnDied -= HandleDied;
     }
 
@@ -48,7 +48,10 @@ public class EnemyController : MonoBehaviour
     private void HandleAttacked()
     {
         if (_isDead) return;
-        Die();
+        _isDead = true;
+        _hitboxCollider.enabled = false;
+        _damagedVFX.Play();
+        Destroy(gameObject, 0.05f); // невелика затримка, щоб все точно відіграло
     }
 
     private void HandleTakeDamage(int newHealth)
