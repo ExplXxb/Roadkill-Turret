3df7b0ec25e6baa37936936bccf3dcaf7eaa2a0a
ExplXxb <n.zemsckij@gmail.com>
2026-09-21 23:35:39 +0300

    fix: hide enemy health bar shortly after death instead of waiting full death animation

--- Assets/Game/Scripts/Shared/UI/HealthBar.cs
+++ Assets/Game/Scripts/Shared/UI/HealthBar.cs
@@ -7,6 +7,7 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Image _fillImage;
     [SerializeField] private Image _backgroundImage;
     [SerializeField] private bool _isShowOnFullHealth;
+    [SerializeField] private float _hideDelayAfterDeath = 0.5f;
 
     private bool _hasTakenDamage;
 
@@ -18,13 +19,25 @@ public class HealthBar : MonoBehaviour
         _hasTakenDamage = false;
 
         _health.OnHealthChanged += UpdateHealthBar;
+        _health.OnDied += HandleDied;
     }
 
     private void OnDisable()
     {
         _health.OnHealthChanged -= UpdateHealthBar;
+        _health.OnDied -= HandleDied;
     }
 
+    private void HandleDied()
+    {
+        Invoke(nameof(Hide), _hideDelayAfterDeath);
+    }
+
+    private void Hide()
+    {
+        _fillImage.enabled = false;
+        _backgroundImage.enabled = false;
+    }
 
     private void UpdateHealthBar(int currentHealth)
     {
